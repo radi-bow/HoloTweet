f89dcc9cad8197520d9bba5e7d0a4a2ed15f481e
Daichi Ito <math144377@gmail.com>
2018-07-12 20:23:51 +0900

    Add GetTweet.cs

---
+++ Holotweet/Assets/Scripts/GetTweet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Twity.DataModels.Responses;
+
+public class GetTweet : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Dictionary<string, string> parameters = new Dictionary<string, string>();
+        parameters["count"] = 30.ToString();
+        parameters["screen_name"] = "radi_bow";
+        StartCoroutine(Twity.Client.Get("statuses/user_timeline", parameters, Callback));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void Callback(bool success, string response)
+    {
+        if (success)
+        {
+            StatusesHomeTimelineResponse Response = JsonUtility.FromJson<StatusesHomeTimelineResponse>(response);
+            for(int i = 0;i< 30; i++)
+            {
+                Debug.Log(Response.items[i].text);
+            }
+        }
+        else
+        {
+            Debug.Log(response);
+        }
+    }
+}
+
