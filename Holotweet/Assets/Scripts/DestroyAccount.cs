748d4d1f87c79ba568ae6dfd64783af8bbecde29
Daichi Ito <math144377@gmail.com>
2018-07-16 23:16:16 +0900

    destroy account when saying "finish"

---
+++ Holotweet/Assets/Scripts/DestroyAccount.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class DestroyAccount : MonoBehaviour, IInputClickHandler
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Destroy(gameObject);
+    }
+
+    /*public void OnDestroy()
+    {
+        Destroy(gameObject);
+    }*/
+}
---
+++ Holotweet/Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,46 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    public GazeManager gazeManager;
+
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start()
+    {
+        keywords.Add("Finish", () =>
+        {
+            var focusObject = gazeManager.HitObject;
+            if (focusObject != null)
+            {
+                // Call the OnDrop method on just the focused object.
+                if (focusObject.CompareTag("Account"))
+                {
+                    Destroy(focusObject.transform.root.gameObject);
+                }
+            }
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
